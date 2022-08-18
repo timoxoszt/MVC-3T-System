8b6ab2a03a460c75c6848765f3e955385d0c6557
timoxoszt <20520810@gm.uit.edu.vn>
2022-08-18 23:49:19 +0700

    Create project

---
+++ src/main/java/com/likelion/mvc3t/Mvc3tApplication.java
@@ -0,0 +1,13 @@
+package com.likelion.mvc3t;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mvc3tApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mvc3tApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/likelion/mvc3t/Mvc3tApplicationTests.java
@@ -0,0 +1,13 @@
+package com.likelion.mvc3t;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mvc3tApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
